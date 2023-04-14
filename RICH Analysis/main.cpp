843223b4a354e55b12ed54f7bc2cf641809bc0a2
CanaanShaw <canaanshaw@icloud.com>
2023-04-14 14:03:26 +0800

    Initial Commit

---
+++ RICH Analysis/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  RICH Analysis
+//
+//  Created by Canaan Shaw on 2023/4/14.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
