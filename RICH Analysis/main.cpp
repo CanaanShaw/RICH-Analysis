b5ace4d859fb81b7395745932f5e2693e64a0e91
CanaanShaw <canaanshaw@icloud.com>
2023-06-09 15:40:30 +0800

    improved details of geographical reconstruction method, now there're basically no bugs

--- RICH Analysis/main.cpp
+++ RICH Analysis/main.cpp
@@ -34,6 +34,7 @@ int main(int argc, const char * argv[]) {
 	const double dx = 0.09;
 	const double dy = -0.075;
 	
+	// Start processing data
 	for (int iEvent = 0; iEvent < tree.GetEntries(); iEvent ++) {
 		
 		tree.GetEntry(iEvent);
@@ -69,53 +70,61 @@ int main(int argc, const char * argv[]) {
 			
 			double maxWeight = 0.0;
 			double theta_c = 0.0;
+			double theta = atan(distance(center, radCenter) / RichConst::aglTransmissionHeight());
+			double phi = atan((centerB - radB) / (centerA - radA));
+			if (centerA - radA < 0) phi += TMath::Pi();
 			
 			// Find the most possible Cherenkov angle theta_i
-			for (double theta_i = 0.2; theta_i < 0.4; theta_i += 0.01) {
+			for (double theta_i = 0.1; theta_i < 0.4; theta_i += 0.005) {
 				
-				double semiMajorAxis = 0.5 * RichConst::aglTransmissionHeight() * (tan(tree.trTheta + theta_i) - tan(tree.trTheta - theta_i));
-				double semiMinorAxis = RichConst::aglTransmissionHeight() / cos(tree.trTheta) * tan(theta_i);
+				// Compute parameters of ellipse
+				double semiMajorAxis = 0.5 * RichConst::aglTransmissionHeight() * (tan(theta + theta_i) - tan(theta - theta_i));
+				double semiMinorAxis = RichConst::aglTransmissionHeight() / cos(theta) * tan(theta_i);
 				
-				// Compute the center of the ellipse
-//				vector < double > ellipseCenter {centerA + semiMajorAxis * cos(tree.trPhi), centerB + semiMajorAxis * sin(tree.trPhi)};
-				double rad2Center = RichConst::aglTransmissionHeight() * tan(tree.trTheta - theta_i) + semiMajorAxis;
-				double phi = atan((centerB - radB) / (centerA - radA));
-				if (centerA - radA < 0) phi += TMath::Pi();
+				// Distance from radCenter to ellipseCenter from top view.
+				double rad2Center = RichConst::aglTransmissionHeight() * tan(theta - theta_i) + semiMajorAxis;
+				
 				vector < double > ellipseCenter {radA + rad2Center * cos(phi) , radB + rad2Center * sin(phi)};
-//				cout << "(" << radA << ", " << radB << "), " << "(" << centerA << ", " << centerB << "), " << "(" << ellipseCenter[0] << ", " << ellipseCenter[1] << ")\n";
-//				cout << "(" << radA << ", " << radB << "), " << "(" << centerA << ", " << centerB << "), " << tree.trPhi << endl;
-//				double rad2Center = distance(radCenter, ellipseCenter);
-//				cout << (ellipseCenter[0] - centerA) / (centerA - radA) << endl;
-//				cout << rad2Center - distance(radCenter, center) << endl;
+				if ((ellipseCenter[0] - centerA) / (centerA - radA) < 0) cout << "Wrong\n";
+				if ((ellipseCenter[1] - centerB) / (centerB - radB) < 0) cout << "Wrong\n";
 				
 				// Compute the likelihood of theta_i
 				double weight = 1.0;
+				double hitsAvailable = hits.size();
+				double inliers = 0;
 				for (int i = 0; i < hits.size(); i ++) {
 					
 					double hit2Rad = distance(radCenter, hits[i]);
 					double hit2Center = distance(ellipseCenter, hits[i]);
 //					cout << fabs(distance(ellipseCenter, center)) << endl;
 					
-					if (hit2Center > 25.0) continue;
+					if (hit2Center > 25.0) {
+						hitsAvailable --;
+						continue;
+					}
+					
 					// Angle phi being radCenter-ellipseCenter-hit
 					double cosPhi = (square(rad2Center) + square(hit2Center) - square(hit2Rad)) / 2.0 / rad2Center / hit2Center;
 					
 					// rExpected being the expected distance of the point on the ellipse to the center, which is on the direction of photon hit to center.
 					double rExpected = sqrt(square(semiMajorAxis * cosPhi) + square(semiMinorAxis * sqrt(1.0 - cosPhi * cosPhi)));
 					weight *= computeWeight2(hit2Center, rExpected);
+					if (fabs(rExpected - hit2Center) < 3) inliers ++;
 				}
+				
+				if (inliers < 5) continue;
+				if (hitsAvailable * 1.0 / hits.size() < 0.5 || hitsAvailable < 5) continue;
 				
 				if (maxWeight < weight) {
 					maxWeight = weight;
 					theta_c = theta_i;
 				}
 			}
+			
+//			if (theta_c == 0) continue;
 
-			using namespace TMath;
 			double n2 = refractiveMap -> GetBinContent(refractiveMap -> FindBin(radA, radB));
 			n2 = 1.055;
-//			double Theta = ATan(tree.recR / RichConst::aglTransmissionHeight());
-//			tree.recBeta = 1.0 / n2 / cos(Theta);
 			tree.recBeta = 1.0 / n2 / cos(theta_c);
 			tree.recMass = tree.trRigidity * tree.trInnerCharge / tree.recBeta * sqrt(1.0 - square(tree.recBeta));
 
